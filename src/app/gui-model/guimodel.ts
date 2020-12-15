09f42d23bfaee2a286a2d455e0c0c20ba8a2b993
libischr <libischr@students.zhaw.ch>
2020-12-15 11:10:06 +0000

    title change

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Christoph Libiszewskis Friend Tracker 1",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -26,6 +26,20 @@ export class GuiModel {
                             "name": "FirstName",
                             "width": 1,
                             "required": true
+                        },
+                        { 
+                            "id": "nickname",
+                            "type": "text", 
+                            "name": "Nickname",
+                            "width": 2,
+                            "required": true 
+                        },
+                        { "id": "group",
+                         "type": "autocomplete",
+                          "name": "Group",
+                          "url": "/group",
+                          "form": "GroupForm",
+                           "width": 2 
                         },
                         {
                             "id":   "location",
@@ -63,6 +77,7 @@ export class GuiModel {
                         }
                     ]
                 },
+                { "id": "GroupForm", "title": "Group", "url": "/group", "formFieldList": [ { "id": "name", "type": "text", "name": "GroupName", "width": 2, "required": true }, { "type": "deleteButton", "name": "Delete" }, { "type": "cancelButton", "name": "Cancel" }, { "type": "okButton", "name": "Ok" } ] },
                 {
                     "id": "LocationForm",
                     "title": "Location",
@@ -138,8 +153,34 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+                        { "type": "button", "name": "Groups", "icon": "fa-weixin", "color": "wisteria", "page": "groupspage", },
                     ]
                 },
+                { 
+                    "id": "groupspage", 
+                    "elementList": [ 
+                        { "type": "backbutton", }, 
+                        { "type": "newButton", 
+                        "name": "NewGroup", 
+                        "icon": "fa-weixin", 
+                        "color": "green", 
+                        "form": { 
+                            "form": "GroupForm" }
+                         }, 
+                        {
+                            "type": "list",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "search": true,
+                            "url": "/group",
+                            "form": {
+                                "form": "GroupForm"
+                            }
+                        },
+                    ]
+                        
+                    },
+                    
                 {
                     "id": "friendspage",
                     "elementList": [
